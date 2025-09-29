488e48dcfdab0559435c0731482b3e2be6663a5e
PeachyChrystal <yxie389@uwo.ca>
2025-09-28 21:44:08 -0400

    backend (deployed)

---
+++ socialai/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	USER_INDEX = "user"
+	POST_INDEX = "post"
+	ES_URL = "http://136.115.185.134:9200"
+	ES_USERNAME = "chrystal"
+	ES_PASSWORD = "Chrystal666!"
+	GCS_BUCKET = "socialai_chrystal_bucket"
+)
+
